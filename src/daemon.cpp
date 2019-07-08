3af4cb89768cc4808f905256ef31825b6116bb10
Daniel Jimenez Gutierrez <danieljimenez92@gmail.com>
2019-07-08 17:35:43 -0500

    quickfix 2

--- src/daemon.cpp
+++ src/daemon.cpp
@@ -25,6 +25,16 @@
 
 using namespace alpr;
 
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm ;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+
 // Variables
 SafeQueue<cv::Mat> framesQueue;
 
